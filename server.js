422591e93a0b0d963c3655fa0cc38abe271e3127
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-21 00:23:47 -0500

    updated env

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const app = express()
-
+require('dotenv').config()
 const { PORT = 3000, NODE_ENV = "development" } = process.env;
 
-//MONGO CONNECTION
-// const mongoose = require("./DB/conn");
+const mongoose = require("./db/connection");
 
 //CORS
 const cors = require("cors");
-// const corsOptions = require("./configs/cors.js");
+
+const express = require('express')
+const app = express()
 
 //OTHER IMPORTS
 const morgan = require("morgan");
 const stateRouter = require("./controllers/stateRoutes");
 const capitalRouter = require("./controllers/capitalRoutes");
 
-////////////
 //MIDDLEWARE
-////////////
 NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
@@ -30,10 +27,6 @@ app.get("/", (req, res) => {
 
 app.use("/capital", capitalRouter);
 app.use("/state", stateRouter);
-
-require('dotenv').config()
-
-
 
 app.listen(PORT, () => {
     console.log(`express on port ${PORT}`)
